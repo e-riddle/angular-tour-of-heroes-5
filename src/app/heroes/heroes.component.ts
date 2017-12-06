2f8de495b1cf9742acd6ceb11739af3a5d9afd4b
Eric Riddle <eriddle@seic.com>
2017-12-06 17:30:39 -0500

    add routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -33,13 +33,14 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
 
-
+  /*
   selectedHero: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-
+  */
+  
   getHeroes(): void {
     //this.heroes = this.heroService.getHeroes();
 
